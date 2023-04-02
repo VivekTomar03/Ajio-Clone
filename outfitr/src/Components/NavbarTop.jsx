e2fdbc3b276b4926340dd29716ddd992cc01d5c6
Your Name <you@example.com>
2023-04-02 18:20:17 +0530

    akash

--- outfitr/src/Components/NavbarTop.jsx
+++ outfitr/src/Components/NavbarTop.jsx
@@ -1,8 +1,27 @@
-import { Box, Container, Flex } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, Container, Flex, useDisclosure } from "@chakra-ui/react";
+import React, { useState } from "react";
 import Login from "../Pages/Login";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useToast } from '@chakra-ui/react'
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
 const NavbarTop = () => {
+  const nav=useNavigate()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [email, setemail] = useState();
+  const [password, setpassword] = useState();
+  let Handlelogin=(e)=>{
+    e.preventDefault()
+  nav("/adminPage")
+  onClose()
+}
   return (
     <Container maxW={"100%"}>
       <Flex w={["90%", "90%", "900%", "90%"]} m={4} gap={10} justify="flex-end">
@@ -16,18 +35,18 @@ const NavbarTop = () => {
           <Login /> / Join OUTFITR
         </Box>
         <Link to={"/mensproduct"}>
-        <Box
-          fontSize={".9rem"}
-          fontFamily="monospace"
-          fontWeight="bold"
-          cursor={"pointer"}
-          opacity={0.7}
-          mt={2}
-        >
-          Customer Care
-        </Box>
+          <Box
+            fontSize={".9rem"}
+            fontFamily="monospace"
+            fontWeight="bold"
+            cursor={"pointer"}
+            opacity={0.7}
+            mt={2}
+          >
+            Customer Care
+          </Box>
         </Link>
-        <Link to={"/adminPage"}>
+        <Button onClick={onOpen} background="white" _hover={{ bg: "white" }}>
           <Box
             fontSize={".9rem"}
             fontFamily="monospace"
@@ -43,8 +62,54 @@ const NavbarTop = () => {
           >
             Visit AdminPage
           </Box>
-        </Link>
+        </Button>
       </Flex>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody padding="15%" mt="auto">
+            <h1
+              class="login-title"
+              style={{
+                textAlign: "center",
+                paddingBottom: "20px",
+                fontSize: "30px",
+                fontWeight: "bold",
+              }}
+            >
+              Admin Login
+            </h1>
+            <form class="login-form">
+              <div>
+                <label for="email">Email </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="me@example.com"
+                  name="email"
+                  required
+                  onChange={e=>setemail(e.target.value)} 
+                />
+              </div>
+              <div>
+                <label for="password">Password </label>
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  required
+                  onChange={e=>setpassword(e.target.value)}
+                />
+              </div>
+              <Button background="black" _hover={{ bg: "black" }} onClick={Handlelogin} color="white">
+                Log in
+              </Button>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Container>
   );
 };
